f3f243559e08641bd0f99e87498440ec8ed8c3f7
SneakyBelg <43534278+SneakyBelg@users.noreply.github.com>
2019-09-04 14:10:54 +0200

    Added django project and created url parameter and basic views

---
+++ InfractionsDjango/mysite/infractions/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render;
+from django.http import HttpResponse;
+import json;
+
+json_data = open('./infractions.json')
+data1 = json.load(json_data)
+data2 = json.dumps(data1)
+json_data.close()
+
+def index(request): 
+    return HttpResponse("Hello! Please enter a min number of speed infractions in th above URL!")
+
+def requestedInfraction(request, numberInfractions): 
+    return HttpResponse("Hello! Please enter a min number of speed infractions in th above URL!")
+

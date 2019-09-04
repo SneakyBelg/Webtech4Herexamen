b17bc37bc1a91b6f41d1dba218157420231e914b
SneakyBelg <43534278+SneakyBelg@users.noreply.github.com>
2019-09-04 14:26:00 +0200

    finished django project

--- InfractionsDjango/mysite/infractions/views.py
+++ InfractionsDjango/mysite/infractions/views.py
@@ -7,9 +7,11 @@ data1 = json.load(json_data)
 data2 = json.dumps(data1)
 json_data.close()
 
+
+
+
 def index(request): 
     return HttpResponse("Hello! Please enter a min number of speed infractions in th above URL!")
 
 def requestedInfraction(request, numberInfractions): 
-    return HttpResponse("Hello! Please enter a min number of speed infractions in th above URL!")
-
+    return HttpResponse([obj for obj in data1 if(obj['infractions_speed'] >= numberInfractions)])
